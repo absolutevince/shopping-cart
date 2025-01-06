98bd27b63f92ad2e0b7b7c901c9a21eac7a59a74
vince <187113297+absolutevince@users.noreply.github.com>
2025-01-06 19:42:47 +0800

    Add getGamesList that fetches games list from the API

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+import { useState, useEffect } from "react";
 import "./styles/reset.css";
 import { Outlet } from "react-router-dom";
 import Header from "./components/header/Header";
 import globalcss from "./styles/global.module.css";
+import getGamesList from "./api/getGamesList.js"
 
 function App() {
+  const [gamesList, setGamesList] = useState()
+
+  useEffect(()=> {
+  const get = async() => {
+      const data = await getGamesList()
+      setGamesList(data)
+    }
+
+    get()
+  }, [])
+
+
   return (
     <div data-testid="app" className={globalcss.root}>
       <Header />
